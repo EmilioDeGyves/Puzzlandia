a0af352a704253ed68c1301da07895bfc36c6215
EmilioDeGyves <emiliodgg.contacto@gmail.com>
2023-04-06 16:18:58 -0600

    cambie los archivos de draggable item y slot para una prueba distinta, se corrigieron algunos errores

---
+++ Assets/Scripts/Mecanicas/Slot.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop/posicion final del objeto");
+
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
