def76392d51f735841137308ebda2fb30b3bfcb7
Gabriel Ruiz <a00835179@tec.mx>
2023-05-04 12:09:28 -0600

    agregue dos niveles, y puse animaciones

--- Assets/Scripts/Personajes/MainMove.cs
+++ Assets/Scripts/Personajes/MainMove.cs
@@ -7,13 +7,16 @@ public class MainMove : MonoBehaviour
 {
     public Camera cam;
     public float moveSpeed = 3.0f;
+    public Sprite[] playerSprites;
     private Rigidbody2D rb;
     private Vector2 targetPosition;
-    private bool isMoving;
+    private bool isMoving, isAnimating;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         targetPosition = transform.position;
         isMoving = false;
     }
@@ -36,6 +39,7 @@ public class MainMove : MonoBehaviour
 
     void SetTargetPosition()
     {
+        isAnimating = false;
         Vector2 mouseWorldPosition = cam.ScreenToWorldPoint(Input.mousePosition);
         targetPosition = new Vector2(mouseWorldPosition.x, mouseWorldPosition.y);
         isMoving = true;
@@ -46,9 +50,24 @@ public class MainMove : MonoBehaviour
         float step = moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(Vector2.MoveTowards(rb.position, targetPosition, step));
 
+        int indx = 0;
+        Vector2 dir = (rb.position - targetPosition) * -1;
+
+        if(isAnimating == false)
+        {
+            if (Mathf.Abs(dir.y) > Mathf.Abs(dir.x) && dir.y > 0) { indx = 0; }
+            if (Mathf.Abs(dir.y) > Mathf.Abs(dir.x) && dir.y < 0) { indx = 1; }
+            if (Mathf.Abs(dir.y) < Mathf.Abs(dir.x) && dir.x > 0) { indx = 2; }
+            if (Mathf.Abs(dir.y) < Mathf.Abs(dir.x) && dir.x < 0) { indx = 3; }
+            GetComponent<SpriteRenderer>().sprite = playerSprites[indx];
+            isAnimating = true;
+        }
+        animator.SetBool("isMoving", true);
+
         if ((Vector2)transform.position == targetPosition)
         {
-            isMoving = false;
+            isMoving = false; isAnimating = false;
+            animator.SetBool("isMoving", false);
         }
     }
 }
